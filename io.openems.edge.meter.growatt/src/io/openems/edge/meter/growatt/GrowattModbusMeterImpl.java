4aa1b83b512ad882eabd502d916806dd37c7e55f
Andrei LAZAROV <andrei_lazarov@yahoo.com>
2025-09-30 10:53:26 +0300

    growatt

---
+++ io.openems.edge.meter.growatt/src/io/openems/edge/meter/growatt/GrowattModbusMeterImpl.java
@@ -0,0 +1,86 @@
+package io.openems.edge.meter.growatt;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.common.types.MeterType;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC4ReadInputRegistersTask;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.meter.api.ElectricityMeter;
+
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_MINUS_1;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "Meter.Growatt", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+public class GrowattModbusMeterImpl extends AbstractOpenemsModbusComponent implements GrowattModbusMeter, ElectricityMeter, ModbusComponent, OpenemsComponent {
+	@Reference
+	private ConfigurationAdmin cm;
+
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	private Config config = null;
+
+	public GrowattModbusMeterImpl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ElectricityMeter.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				GrowattModbusMeter.ChannelId.values() //
+		);
+	}
+
+	@Activate
+	private void activate(ComponentContext context, Config config) throws OpenemsException {
+		if(super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm, "Modbus",
+				config.modbus_id())) {
+			return;
+		}
+		this.config = config;
+	}
+
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() {
+		return new ModbusProtocol(this, //
+				new FC4ReadInputRegistersTask(36, Priority.HIGH,
+						m(ElectricityMeter.ChannelId.ACTIVE_POWER, new SignedWordElement(36), SCALE_FACTOR_MINUS_1)));
+	}
+
+	@Override
+	public String debugLog() {
+		return "mbL:" + this.getActivePower().asString();
+	}
+
+	@Override
+	public MeterType getMeterType() {
+		return this.config.type();
+	}
+}
