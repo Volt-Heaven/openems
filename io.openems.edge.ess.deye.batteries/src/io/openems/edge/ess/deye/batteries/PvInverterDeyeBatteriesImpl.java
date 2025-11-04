dfb5c8790b68e7876ffa307d56e8c6f84f8cc06f
Andrei LAZAROV <andrei_lazarov@yahoo.com>
2025-11-04 20:12:35 +0200

    Deye batteries

---
+++ io.openems.edge.ess.deye.batteries/src/io/openems/edge/ess/deye/batteries/PvInverterDeyeBatteriesImpl.java
@@ -0,0 +1,109 @@
+package io.openems.edge.ess.deye.batteries;
+
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_1;
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_2;
+import static io.openems.edge.bridge.modbus.api.ElementToChannelConverter.SCALE_FACTOR_3;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.metatype.annotations.Designate;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.modbusslave.ModbusSlave;
+import io.openems.edge.common.modbusslave.ModbusSlaveNatureTable;
+import io.openems.edge.common.modbusslave.ModbusSlaveTable;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.ess.api.AsymmetricEss;
+import io.openems.edge.ess.api.SymmetricEss;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "ESS.Deye.Batteries", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+public class PvInverterDeyeBatteriesImpl extends AbstractOpenemsModbusComponent 
+		implements PvInverterDeyeBatteries, ModbusComponent, ModbusSlave, OpenemsComponent, SymmetricEss {
+
+	@Reference
+	private ConfigurationAdmin cm;
+
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	private Config config = null;
+
+	public PvInverterDeyeBatteriesImpl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				PvInverterDeyeBatteries.ChannelId.values(), //
+				SymmetricEss.ChannelId.values() //
+		);
+	}
+
+	@Activate
+	private void activate(ComponentContext context, Config config) throws OpenemsException {
+		if(super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm, "Modbus",
+				config.modbus_id())) {
+			return;
+		}
+		this._setCapacity(config.capacity());
+		this.config = config;
+	}
+
+	@Override
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() {
+		// TODO implement ModbusProtocol
+		
+		return new ModbusProtocol(this,
+				new FC3ReadRegistersTask(590, Priority.HIGH,
+						m(SymmetricEss.ChannelId.ACTIVE_POWER, new SignedWordElement(590), SCALE_FACTOR_1)
+				),
+				new FC3ReadRegistersTask(588, Priority.HIGH, 
+						m(SymmetricEss.ChannelId.SOC, new UnsignedWordElement(588))
+				)
+				
+		);
+	}
+
+	@Override
+	public String debugLog() {
+		return "Hello World";
+	}
+	
+	@Override
+	public ModbusSlaveTable getModbusSlaveTable(AccessMode accessMode) {
+		return new ModbusSlaveTable(//
+				OpenemsComponent.getModbusSlaveNatureTable(accessMode), //
+				SymmetricEss.getModbusSlaveNatureTable(accessMode), //
+				AsymmetricEss.getModbusSlaveNatureTable(accessMode), //
+				ModbusSlaveNatureTable.of(PvInverterDeyeBatteries.class, accessMode, 300) //
+						.build());	//TODO_ANDY don't know what this is
+	}
+}
